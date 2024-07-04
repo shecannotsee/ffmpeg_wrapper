9dca86ca799c67615327c6ae4f9ce227cd66611a
shecannotsee <shecannotsee@163.com>
2024-07-04 20:41:34 +0800

    [chore] 1.Add push_flow.sh

---
+++ scripts/push_flow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#!/bin/bash
+project="ffmpeg_wrapper"
+project_length=${#project}
+# Get pwd string
+path=$(pwd)
+# Get last project_length chars
+suffix="${path: -$project_length}"
+if [ "$suffix" = "$project" ]; then
+echo "ok"
+else
+echo "Path error.Please ensure that the last few strings after using pwd are "$project
+exit 1  # stop
+fi
+
+# rtsp-server
+# wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.7/rtsp-simple-server_v0.17.7_linux_amd64.tar.gz
+# tar xvf rtsp-simple-server_v0.17.7_linux_amd64.tar.gz
+# ./rtsp-simple-server
+ffmpeg -re -i ./resource/KTMR2R.mp4 -c copy -f rtsp rtsp://localhost:8554/mystream
+
+
