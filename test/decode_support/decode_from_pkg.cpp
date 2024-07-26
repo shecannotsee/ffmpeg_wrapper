9678d6c57186beff15701a3e80e1cf28721abf7f
shecannotsee <shecannotsee@163.com>
2024-07-26 10:39:59 +0800

    [feat] 1.添加解码器独立的解码 2.配合rtsp_buffer实现分段解码并保存为图片

---
+++ test/decode_support/decode_from_pkg.cpp
@@ -0,0 +1,64 @@
+#include <decode_support.h>
+#include <she_log.h>
+#include <she_test.h>
+
+#include <gsl/util>
+
+#include "rtsp_buffer.h"
+#include "save_frame_as_jpeg.h"
+#include "test_support.h"
+
+SHE_TEST(decode_support, decode_from_pkg) {
+  const std::string rtsp_url   = "rtsp://localhost:8554/mystream";
+  const std::string output_dir = "./decode_support/pkg/";
+  test_support::create_dir(output_dir);
+
+  // 独立解码器设置
+  pre_decoding rtsp_stream;
+  rtsp_stream.set_format_from(rtsp_url);
+  decoding<AVCodecID> decoder(rtsp_stream.get_video_type());
+
+  // 获取缓存,时间为10s
+  rtsp_buffer stream;
+  stream.start_buffering(rtsp_url);
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  stream.stop_buffering();
+  auto bufffer = stream.get_buffer();
+
+  int frame_num = 0;
+  // 使用解码器进行解码
+  for (auto gop : bufffer) {
+    // 组装 std::vector<AVPacket*>
+    std::vector<AVPacket*> packets;
+    auto packets_release = gsl::finally([&]() {
+      for (auto& packet : packets) {
+        av_packet_free(&packet);
+      }
+    });
+    for (const auto& packet_info : gop.packet) {
+      AVPacket* temp;
+      temp = av_packet_alloc();
+      temp->data = static_cast<uint8_t*>(av_malloc(packet_info.pkg_data.size()));
+
+      memcpy(temp->data, packet_info.pkg_data.data(), packet_info.pkg_data.size());
+      temp->size = static_cast<int>(packet_info.pkg_data.size());
+      temp->pts  = packet_info.pts;
+      temp->dts  = packet_info.dts;
+      packets.emplace_back(temp);
+    }
+    auto frames         = decoder.independent_decoder(gop.extradata, packets);
+    auto frames_release = gsl::finally([&]() {
+      for (int i = 0; i < frames.size(); i++) {
+        av_frame_free(&frames[i]);
+      }
+    });
+
+    for (int i = 0; i < frames.size(); i++) {
+      save_frame_as_jpeg(frames[i], frames[i]->width, frames[i]->height,
+                         output_dir + std::to_string(++frame_num) + ".jpg");
+    }
+  }
+
+
+  return true;
+}
