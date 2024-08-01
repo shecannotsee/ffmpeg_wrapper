0a5de0f08ea5737b5105665e79096a96ae8f6268
shecannotsee <shecannotsee@163.com>
2024-08-01 11:51:00 +0800

    [test] 1.对指定pts画框的整体测试流程

--- test/frame_align/error_with_flows.cpp
+++ test/frame_align/error_with_flows.cpp
@@ -4,6 +4,7 @@
 
 #include "decode_support.h"
 #include "draw_on_the_picture.h"
+#include "encode_support.h"
 #include "rtsp_buffer.h"
 #include "save_frame_as_jpeg.h"
 #include "test_support.h"
@@ -35,7 +36,8 @@ SHE_TEST(frame_align, error_with_flows) {
   };
   tag draw_target;
   // 从buffer_1中获取第三组的所有frame的pts，试图在buffer_2中定位并进行绘制
-  int num = 0;
+  int num                  = 0;
+  constexpr int num_target = 3;
   for (auto gop : bufffer_1) {
     num++;
     // 组装 std::vector<AVPacket*>
@@ -56,7 +58,7 @@ SHE_TEST(frame_align, error_with_flows) {
       temp->dts  = packet_info.dts;
       packets.emplace_back(temp);
     }
-    if (num == 3) {
+    if (num == num_target) {
       // 送入解码器
       auto frames         = decoder.independent_decoder(gop.extradata, packets);
       auto frames_release = gsl::finally([&]() {
@@ -74,8 +76,58 @@ SHE_TEST(frame_align, error_with_flows) {
   }
 
   // 在buffer_2中查找并绘制
-  auto first_pts = draw_target.pts;
-  for (auto gop : bufffer_2) {
+  // 首先获取偏移量
+  int offset = 0;
+  {
+    // 获取要绘制的
+    auto first_pts                  = draw_target.pts[0];
+    int first_bigger_than_first_pts = 0;
+    for (auto gop : bufffer_2) {
+      for (auto packet_info : gop.packet) {
+        if (packet_info.dts == first_pts) {
+          offset = 0;
+          break;
+        } else if (packet_info.dts > first_pts) {  // 找到了第一个大于画框组的帧的pts就可以开始计算offset了
+          first_bigger_than_first_pts = packet_info.dts;
+          offset                      = first_bigger_than_first_pts - first_pts;
+          break;
+        }
+      }
+      if (offset != 0) {
+        break;
+      }
+    }
+  }
+  // 获取需要解码和绘制的组
+  int start_gop_num = 0;
+  int end_top_num   = 0;
+  {
+    int draw_start_pts = draw_target.pts[0] + offset;
+    int draw_end_pts   = draw_target.pts.back() + offset;
+    for (int i = 0; i < bufffer_2.size(); ++i) {
+      for (auto pkg : bufffer_2[i].packet) {
+        if (draw_start_pts == pkg.dts) {
+          start_gop_num = i;
+        }
+        if (draw_end_pts == pkg.dts) {
+          end_top_num = i;
+          break;
+        }
+      }
+      if (end_top_num != 0) {
+        break;
+      }
+    }
+  }
+
+  // 对指定的组进行解码
+  std::vector<AVFrame*> frames{};
+  auto frames_release = gsl::finally([&]() {
+    for (int i = 0; i < frames.size(); i++) {
+      av_frame_free(&frames[i]);
+    }
+  });
+  for (int i = start_gop_num; i <= end_top_num; ++i) {
     // 组装 std::vector<AVPacket*>
     std::vector<AVPacket*> packets;
     auto packets_release = gsl::finally([&]() {
@@ -83,7 +135,7 @@ SHE_TEST(frame_align, error_with_flows) {
         av_packet_free(&packet);
       }
     });
-    for (const auto& packet_info : gop.packet) {
+    for (const auto& packet_info : bufffer_2[i].packet) {
       AVPacket* temp;
       temp       = av_packet_alloc();
       temp->data = static_cast<uint8_t*>(av_malloc(packet_info.pkg_data.size()));
@@ -94,11 +146,40 @@ SHE_TEST(frame_align, error_with_flows) {
       temp->dts  = packet_info.dts;
       packets.emplace_back(temp);
     }
-    // 开始校准
-    int offset = (packets[0]->dts % 100) - (first_pts % 100);
-    for (auto packet : packets) {
+    // 解码
+    auto temp          = decoder.independent_decoder(bufffer_2[i].extradata, packets);
+    for (auto frame : temp) {
+      frames.emplace_back(frame);
     }
   }
+
+  // 绘制
+  for (auto frame : frames) {
+    draw_detection_boxes(frame, get_detections_test());
+  }
+
+  // 最后进行编码和保存
+  {
+    encoding<AVCodecID> encoder(AV_CODEC_ID_H264);
+    auto codec_ctx = rtsp_stream.get_stream_ctx()->streams[rtsp_stream.get_video_stream_index()]->codecpar;
+    std::vector<uint8_t> extradata(codec_ctx->extradata, codec_ctx->extradata + codec_ctx->extradata_size);
+    auto packets         = encoder.get_encode_packets(frames,extradata);
+    auto packets_release = gsl::finally([&]() {
+      for (auto& packet : packets) {
+        av_packet_free(&packet);
+      }
+    });
+    // 将编码数据写入文件
+    {
+      std::ofstream file(output_dir + "output.h264", std::ios::out | std::ios::binary);
+      for (auto packet : packets) {
+        file.write((char*)packet->data, packet->size);
+      }
+      file.close();
+    }
+  }
+
+
 
   return true;
 }
