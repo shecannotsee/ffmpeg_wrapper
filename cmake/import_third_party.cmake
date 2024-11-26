0a1d0e06c77d12281a09fcc63b5befd5a128d3da
shecannotsee <shecannotsee@163.com>
2024-11-26 14:36:16 +0800

    [build] 1.import ffmpeg

--- cmake/import_third_party.cmake
+++ cmake/import_third_party.cmake
@@ -14,4 +14,15 @@ link_directories(${gtest_lib_dir})
 list(APPEND test_link_dependency ${gtest_link_libs})
 ########################################################################################################################
 
-
+########################################################################################################################
+# set ffmpeg
+set(FFMPEG_ROOT_DIR "/usr/local" CACHE STRING "default path")
+set(_ffmpeg_path ${FFMPEG_ROOT_DIR})
+set(ffmpeg_include_dir "${_ffmpeg_path}/include")
+set(ffmpeg_lib_dir "${_ffmpeg_path}/lib")
+set(ffmpeg_link_libs avcodec avdevice avfilter avformat avutil postproc swresample swscale)
+# import ffmpeg
+include_directories(${ffmpeg_include_dir})
+link_directories(${ffmpeg_lib_dir})
+list(APPEND src_link_dependency ${ffmpeg_link_libs})
+########################################################################################################################
