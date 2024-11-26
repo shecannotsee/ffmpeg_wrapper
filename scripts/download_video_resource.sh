ae93e9612227a8623b01fae401980db784b5ce06
shecannotsee <shecannotsee@163.com>
2024-11-26 17:37:47 +0800

    [scripts] 1.Add resource download script

---
+++ scripts/download_video_resource.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+project="ffmpeg_wrapper"
+project_length=${#project}
+# Get pwd string
+path=$(pwd)
+# Get last project_length chars
+suffix="${path: -$project_length}"
+if [ "$suffix" = "$project" ]; then
+    echo "ok"
+else
+    echo "Path error.Please ensure that the last few strings after using pwd are "$project
+    exit 1  # stop
+fi
+
+export HTTP_PROXY="http://127.0.0.1:7890"
+export HTTPS_PROXY="https://127.0.0.1:7890"
+# https://gist.github.com/jsturgis/3b19447b304616f18657?permalink_comment_id=4920424#gistcomment-4920424
+# get source code
+wget -P ./resource http://samples.mplayerhq.hu/V-codecs/h264/NeroAVC.mp4 || \
+  { echo "wget failed"; exit 1; }
