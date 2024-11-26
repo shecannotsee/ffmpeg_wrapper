8dd8a4a974ff3fdaedc8ef56cbe0df1cbef281b3
shecannotsee <shecannotsee@163.com>
2024-11-26 19:18:46 +0800

    [test] 1.Update test

--- test/functional/functional_sample.cpp
+++ test/functional/functional_sample.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
-#include <cal.h>
 
 TEST(functional, sample) {
-  EXPECT_EQ(cal<int>::add(2,3), 5);
+  EXPECT_EQ(5, 5);
 }
--- test/integration/integration_sample.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include <cal.h>
-
-TEST(integration, sample) {
-  EXPECT_EQ(cal<int>::add(2,3), 5);
-}
